c9f1b75105a29f9733662576f8caaae4238d5b80
xliang <lxq520064@126.com>
2019-04-21 12:30:26 +0000

    kubeadm使用

---
+++ kubeadm/install_kubeadm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# 安装kubeadm
+# 参考资料: https://kubernetes.io/docs/setup/independent/install-kubeadm/
+
+# 需要翻墙，也可改用下面的aliyun的镜像仓库
+#cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+#[kubernetes]
+#name=Kubernetes
+#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+#enabled=1
+#gpgcheck=1
+#repo_gpgcheck=1
+#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+#exclude=kube*
+#EOF
+
+# 配置源
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
